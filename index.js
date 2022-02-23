870d390aac39c912ce2ad1075a663bb8a17a10d2
+ <+>
2022-02-23 15:21:32 -0700

    commit tree 56815b3abc5c447e254c615f911a463afc2d95fb

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your code here
+/* // Add your code here
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+    body: JSON.stringify({
+            dogName: "Byron",
+            dogBreed: "Poodle"
+    })
+}
+
+fetch('http://localhost:3000/dogs', configurationObject)
+.then(res => res.json())
+.then(data => console.log(data)) */
+
+function submitData(userName, userEmail) {
+    const submitDataPost = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }
+
+    return fetch('http://localhost:3000/users', submitDataPost)
+    .then(response => response.json())
+    .then(userInfo => {
+        //console.log(userInfo.id)
+        let p = document.createElement('p')
+        p.textContent = userInfo.id
+        return document.querySelector('body').appendChild(p)
+    })
+    .catch(error => {
+        let p = document.createElement('p')
+        p.textContent = error
+        return document.querySelector('body').appendChild(p)
+    })
+}
+
+submitData("Andrew", "sample@sample.org")
+
